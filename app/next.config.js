dabc7a2e2c3209c2ad9d53b71f3316e0fe2291a2
Muhammad Hashfi Hadyan <116771617+hshinosa@users.noreply.github.com>
2026-01-03 22:58:26 +0700

    feat: kubernetes deployment for MySQL and Next.js

--- app/next.config.js
+++ app/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
   outputFileTracingRoot: __dirname,
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000',
